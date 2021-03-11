77259c195cada4e23f0439e229984f6e85c4b0d1
jamesworden <jamesworden.ny@gmail.com>
2021-03-11 12:08:40 -0500

    Working with env variables

--- src/route/routes.js
+++ src/route/routes.js
@@ -39,7 +39,8 @@ routes.get('/', async function (req, res) {
 	let key = process.env.GOOGLE_MAPS_API_KEY;
 	// Key is undefined, check AWS Secrets for API Key
 	if (key == undefined) {
-		key = secretManager.getSecret('GOOGLE_MAPS_API_KEY');
+		// key = secretManager.getSecret('GOOGLE_MAPS_API_KEY');
+		key = env.GOOGLE_MAPS_API_KEY;
 		// Testing
 		console.log('Route.js secret: ' + key);
 	}
