5f27e3377ce8e8d333ce083b71d3b3108ce78705
jamesworden <jamesworden.ny@gmail.com>
2021-03-11 15:38:07 -0500

    Commented code on index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+// Created by James Worden
+// GitHub: jamesworden
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const { routes: routeRoutes } = require('./route/routes');
+// Import routes
+const { routes: routeRoutes } = require('./route/routes'); // Get routes of type route
 
+// Create express app
 const app = express();
 
+// Initialize middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/route', routeRoutes);
 
+// Export app
 module.exports = app;
