956862008c4b7dc505db8de5f424dd8c93863ff8
jamesworden <jamesworden.ny@gmail.com>
2021-03-14 19:08:39 -0400

    Added map to frontend; able to submit form on frontend

--- src/frontend/routes.js
+++ src/frontend/routes.js
@@ -8,8 +8,13 @@ const routes = express.Router({
 
 // Dynamically rendering the enviornment variables into the ejs template
 routes.get('/', async function (req, res) {
+	// Get origin and destination values from queries
+	// The front end does not require parameters; the API does
 	res.render('index.ejs', {
 		GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+		origin: req.query.origin || '8000 Utopia Pkwy, Jamaica, NY 11439',
+		destination: req.query.destination || '2, 168-46 91st Ave, 11432',
+		increment: req.query.increment || 10, // Default 10 meters
 	});
 });
 
