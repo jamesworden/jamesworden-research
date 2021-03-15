046fde5cc49384708d98cd943156cba8d316869a
jamesworden <jamesworden.ny@gmail.com>
2021-03-15 01:50:14 -0400

    Reformatting JSON data for waypoints

--- src/frontend/routes.js
+++ src/frontend/routes.js
@@ -8,13 +8,29 @@ const routes = express.Router({
 // Dynamically rendering the enviornment variables into the ejs template
 routes.get('/', async function (req, res) {
 	// Get origin and destination values from queries
-	// The front end does not require parameters; the API does
-	res.render('index.ejs', {
-		GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
-		origin: req.query.origin || '8000 Utopia Pkwy, Jamaica, NY 11439',
-		destination: req.query.destination || '2, 168-46 91st Ave, 11432',
-		increment: req.query.increment || 10, // Default 10 meters
-	});
+	origin = req.query.origin || '8000 Utopia Pkwy, Jamaica, NY 11439';
+	destination = req.query.destination || '2, 168-46 91st Ave, 11432';
+	increment = req.query.increment || 10; // Default 10 meters
+
+	// Get the route and then render the page
+	const { getRoute } = require('../route/route');
+	getRoute(origin, destination, increment)
+		.then((route) => {
+			// The front end does not require parameters; the API does
+			res.render('index.ejs', {
+				GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+				origin,
+				destination,
+				increment,
+				route,
+			});
+		})
+		.catch(() => {
+			res.status(500).send({
+				error: 'Unable to create complete request!',
+				message: 'Please contact James for assistance.',
+			});
+		});
 });
 
 // Export all the routes
